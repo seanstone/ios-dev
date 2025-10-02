f308747b768dcfd1d13df3875ef34792726fd5b0
En Shih <seanstone5923@gmail.com>
2025-10-02 19:51:19 +0800

    feat: print permissions

--- app/app-main.c
+++ app/app-main.c
@@ -4,6 +4,36 @@
 #include <errno.h>    // errno
 #include <unistd.h>
 #include <string.h>
+#include <sys/stat.h>   // stat()
+
+static void print_permissions(mode_t mode)
+{
+    char perms[11];
+
+    // File type
+    if (S_ISDIR(mode)) perms[0] = 'd';
+    else if (S_ISLNK(mode)) perms[0] = 'l';
+    else perms[0] = '-';
+
+    // Owner
+    perms[1] = (mode & S_IRUSR) ? 'r' : '-';
+    perms[2] = (mode & S_IWUSR) ? 'w' : '-';
+    perms[3] = (mode & S_IXUSR) ? 'x' : '-';
+
+    // Group
+    perms[4] = (mode & S_IRGRP) ? 'r' : '-';
+    perms[5] = (mode & S_IWGRP) ? 'w' : '-';
+    perms[6] = (mode & S_IXGRP) ? 'x' : '-';
+
+    // Others
+    perms[7] = (mode & S_IROTH) ? 'r' : '-';
+    perms[8] = (mode & S_IWOTH) ? 'w' : '-';
+    perms[9] = (mode & S_IXOTH) ? 'x' : '-';
+
+    perms[10] = '\0';
+
+    printf("%s", perms);
+}
 
 void ios_main(const char* cache_dir, const char* bundle_dir, const char* frameworks_dir)
 {
@@ -18,6 +48,18 @@ void ios_main(const char* cache_dir, const char* bundle_dir, const char* framewo
 
     printf("Contents of %s:\n", bundle_dir);
     while ((entry = readdir(dir)) != NULL) {
+        // Build full path for stat
+        char path[1024];
+        snprintf(path, sizeof(path), "%s/%s", bundle_dir, entry->d_name);
+
+        struct stat st;
+        if (stat(path, &st) == -1) {
+            printf("  %-20s [stat error: %s]\n", entry->d_name, strerror(errno));
+            continue;
+        }
+
+        // Print permissions and name
+        print_permissions(st.st_mode);
         printf("  %s\n", entry->d_name);
     }
 
