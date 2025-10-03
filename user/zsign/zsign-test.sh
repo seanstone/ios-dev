18044389fa73ff5666eda3a3028a419128ead1dc
En Shih <seanstone5923@gmail.com>
2025-10-04 03:01:23 +0800

    refactor: install libs to lib dir

--- user/zsign/zsign-test.sh
+++ user/zsign/zsign-test.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
 mkdir -p ../bin/
+mkdir -p ../lib/
 
-cp /opt/homebrew/opt/openssl@3/lib/libssl.3.dylib ../bin/libssl.3.dylib
-../../toolchain/patch_to_ios.py ../bin/libssl.3.dylib -o ../bin/libssl.3.dylib.1
-rm -f ../bin/libssl.3.dylib
-mv ../bin/libssl.3.dylib.1 ../bin/libssl.3.dylib
+cp /opt/homebrew/opt/openssl@3/lib/libssl.3.dylib ../lib/libssl.3.dylib
+../../toolchain/patch_to_ios.py ../lib/libssl.3.dylib -o ../lib/libssl.3.dylib.1
+rm -f ../lib/libssl.3.dylib
+mv ../lib/libssl.3.dylib.1 ../lib/libssl.3.dylib
 
-cp /opt/homebrew/opt/openssl@3/lib/libcrypto.3.dylib ../bin/libcrypto.3.dylib
-../../toolchain/patch_to_ios.py ../bin/libcrypto.3.dylib -o ../bin/libcrypto.3.dylib.1
-rm -f ../bin/libcrypto.3.dylib
-mv ../bin/libcrypto.3.dylib.1 ../bin/libcrypto.3.dylib
+cp /opt/homebrew/opt/openssl@3/lib/libcrypto.3.dylib ../lib/libcrypto.3.dylib
+../../toolchain/patch_to_ios.py ../lib/libcrypto.3.dylib -o ../lib/libcrypto.3.dylib.1
+rm -f ../lib/libcrypto.3.dylib
+mv ../lib/libcrypto.3.dylib.1 ../lib/libcrypto.3.dylib
 
-cp /opt/homebrew/opt/minizip/lib/libminizip.1.dylib ../bin/libminizip.1.dylib
-../../toolchain/patch_to_ios.py ../bin/libminizip.1.dylib -o ../bin/libminizip.1.dylib.1
-rm -f ../bin/libminizip.1.dylib
-mv ../bin/libminizip.1.dylib.1 ../bin/libminizip.1.dylib
+cp /opt/homebrew/opt/minizip/lib/libminizip.1.dylib ../lib/libminizip.1.dylib
+../../toolchain/patch_to_ios.py ../lib/libminizip.1.dylib -o ../lib/libminizip.1.dylib.1
+rm -f ../lib/libminizip.1.dylib
+mv ../lib/libminizip.1.dylib.1 ../lib/libminizip.1.dylib
 
 ../../toolchain/patch_macho.py ./zsign/bin/zsign ../bin/zsign
 ../../toolchain/patch_to_ios.py ../bin/zsign -o ../bin/zsign
-#../../toolchain/patch_add_syslog_shim.py ../bin/libcrypto.3.dylib ../bin/libcrypto.3.dylib
+#../../toolchain/patch_add_syslog_shim.py ../lib/libcrypto.3.dylib ../lib/libcrypto.3.dylib
 
 install_name_tool -change \
   /opt/homebrew/opt/openssl@3/lib/libssl.3.dylib \
@@ -39,26 +40,26 @@ install_name_tool -change \
 install_name_tool -change \
   /opt/homebrew/opt/openssl@3/lib/libcrypto.3.dylib \
   @rpath/libcrypto.3.dylib \
-  ../bin/libcrypto.3.dylib
+  ../lib/libcrypto.3.dylib
 
 install_name_tool -change \
   /opt/homebrew/opt/openssl@3/lib/libssl.3.dylib \
   @rpath/libssl.3.dylib \
-  ../bin/libssl.3.dylib
+  ../lib/libssl.3.dylib
 
 install_name_tool -change \
   /opt/homebrew/Cellar/openssl@3/3.5.4/lib/libcrypto.3.dylib \
   @rpath/libcrypto.3.dylib \
-  ../bin/libssl.3.dylib
+  ../lib/libssl.3.dylib
 
 install_name_tool -change \
   /opt/homebrew/opt/minizip/lib/libminizip.1.dylib \
   @rpath/libminizip.1.dylib \
-  ../bin/libminizip.1.dylib
+  ../lib/libminizip.1.dylib
 
 install_name_tool -change /usr/lib/libSystem.B.dylib \
   @rpath/libsyslog_extsn_shim.dylib \
-  ../bin/libcrypto.3.dylib
+  ../lib/libcrypto.3.dylib
 
 codesign --force --sign "Apple Development: En Shih (8JAX23HR7H)" \
     --options runtime \
@@ -70,16 +71,16 @@ codesign --force --sign "Apple Development: En Shih (8JAX23HR7H)" \
     --options runtime \
     --timestamp=none \
     --preserve-metadata=identifier,flags \
-    ../bin/libssl.3.dylib
+    ../lib/libssl.3.dylib
 
 codesign --force --sign "Apple Development: En Shih (8JAX23HR7H)" \
     --options runtime \
     --timestamp=none \
     --preserve-metadata=identifier,flags \
-    ../bin/libcrypto.3.dylib
+    ../lib/libcrypto.3.dylib
 
 codesign --force --sign "Apple Development: En Shih (8JAX23HR7H)" \
     --options runtime \
     --timestamp=none \
     --preserve-metadata=identifier,flags \
-    ../bin/libminizip.1.dylib
+    ../lib/libminizip.1.dylib
